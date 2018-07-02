541f630a20852b40ec1cf9cae80354ad9b8a6e95
Kevin Barabash <kevinb@khanacademy.org>
2018-07-01 20:39:17 -0400

    make background.js' response asynchronous

--- extension/dist/background.js
+++ extension/dist/background.js
@@ -1,26 +1,31 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.runtime.onMessage.addListener(message => {
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         console.log(message);
-        chrome.windows.getCurrent({}, win => {
-            chrome.tabs.captureVisibleTab(win.id, {format: "png"}, dataUrl => {
-                console.log("taking screenshot");
 
-                // TODO(kevinb): get hostname from content.js script
-                fetch("http://localhost:3000/screenshot", {
-                    method: "POST",
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        dataUrl: dataUrl,
-                        filename: message.filename,
-                        bounds: message.bounds,
-                    }),
-                }).then(() => {
-                    console.log("message sent to /screenshot");
-                });
+        chrome.tabs.captureVisibleTab(sender.tab.windowId, {format: "png"}, dataUrl => {
+            console.log("taking screenshot");
+
+            // tell content.js that we've successfully taken the screenshot
+            sendResponse();
+
+            // TODO(kevinb): get hostname from content.js script
+            fetch("http://localhost:3000/screenshot", {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    dataUrl: dataUrl,
+                    filename: message.filename,
+                    bounds: message.bounds,
+                }),
+            }).then(() => {
+                console.log("message sent to /screenshot");
             });
         });
+
+        // indicate that response will be asynchronous
+        return true;
     });
 });
